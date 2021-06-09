2eacbd773fb671a857fc7afc285c429f1dbb7060
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-06-09 23:11:20 +0530

    tests: Added model tests

--- server/test_models.py
+++ server/test_models.py
@@ -9,6 +9,11 @@ class TestAdminModel(TestCase):
     @staticmethod
     def create_invalid():
         user = Admin('testuser@localhost.com')
-        
+        return user
+
     def test_invalid_entry(self):
-        self.assertRaises(TypeError, )
+        self.assertRaises(TypeError, self.create_invalid)
+
+    def test_repr(self):
+        user = Admin(email='testuser@localhost.com', password='testpassword')
+        self.assertEqual(str(user), 'testuser@localhost.com')
